ac6f520038b6455895dbe876b6020e489b7e970e
Muskan-Nisar <114562777+Muskan-Nisar@users.noreply.github.com>
2024-01-09 21:30:23 +0530

    Add files via upload

---
+++ swap.cpp
@@ -0,0 +1,23 @@
+#include<iostream>
+using namespace std;
+// **************by extra variable*****************
+
+// int main(){
+//     int a;
+//     cin>>a;int b;
+    // cin >>b;
+//     temp=a;
+//     a=b;
+//     b=temp;
+//     cout<<a<<" "<<b;
+
+
+// *********************by another ,method***********
+int main(){
+    int a,b;
+    cin>>a>>b;
+    a=a+b;
+    b=a-b;
+    a=a-b;
+cout<<a<<" "<<b;
+}
