f458c47c85856dc745b953b0ce2ca61d18919fb8
Muskan-Nisar <114562777+Muskan-Nisar@users.noreply.github.com>
2024-02-14 14:02:30 +0530

    Add files via upload

---
+++ mergeSort.cpp
@@ -0,0 +1,64 @@
+#include <iostream>
+#include<vector>
+using namespace std;
+
+void merge(vector<int> &arr,int low,int mid,int high){
+    vector<int> temp;
+    int left= low;
+    int right =mid +1;
+    while(left<=mid && right<=high){
+        if(arr[left]<=arr[right]){
+            temp.push_back(arr[left]);
+            left++;
+        } else {
+            temp.push_back(arr[right]);
+            right++;
+        }
+    }
+        while(left<=mid){
+            temp.push_back(arr[left]);
+            left++;
+    }
+    while(right<=high){
+        temp.push_back(arr[right]);
+        right++;
+    }
+
+    for (int i = low; i <= high; i++) {
+        arr[i] =temp[i - low];
+    }
+}
+
+    void mS(vector<int> &arr, int low, int high) {
+  if (low == high)return;
+  int mid = (low + high) / 2;
+  mS(arr, low, mid);
+  mS(arr, mid + 1, high);
+  merge(arr, low, mid, high);
+}
+
+    void mergeSort(vector < int > & arr, int n) {
+    // Write your code here.
+    mS(arr,0,n-1);
+}
+
+int main() {
+    // Example usage
+int n;
+cin>>n;
+vector<int> v;
+for(int i=0;i<n;i++){
+    int q;
+    cin>>q;
+    v.push_back(q);
+}
+
+    mergeSort(v,n);
+
+    cout << "Sorted array: ";
+    for (int i = 0; i < n; i++) {
+        cout << v[i] << " ";
+    }
+
+    return 0;
+}
