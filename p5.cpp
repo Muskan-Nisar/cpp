759128c2b35e85cea9a7b088f2663a0e5f4016f0
Muskan-Nisar <114562777+Muskan-Nisar@users.noreply.github.com>
2024-02-29 20:43:40 +0530

    Add files via upload

---
+++ p5.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int n;
+    cin>>n;
+    for(int i=0;i<n;i++){
+        for(int j=0;j<n-i;j++){
+            cout<<j+i+1<<" ";
+        }
+        for(int j=1;j<i+1;j++){
+            cout<<j<<" ";
+        }
+        cout<<endl;
+    }
+}
