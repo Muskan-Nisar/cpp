faf8bdded4c033407f1704fe5373b94b52afd394
Muskan-Nisar <114562777+Muskan-Nisar@users.noreply.github.com>
2024-03-09 13:13:49 +0530

    Add files via upload

---
+++ replaceodd.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+#include<string>
+using namespace std;
+int main(){
+    string str;
+    getline(cin,str);
+    for(int i=0;str[i]!='\0';i++){
+        if(i%2!=0){
+            str[i]='#';
+        }
+    }
+    cout<<str;
+}
